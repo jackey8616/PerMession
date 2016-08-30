d967a61d2a048f369cd87c3188a088d4f9c10f07
jackey8616 <jackey8616@gmail.com>
2016-08-30 08:53:31 +0800

    2016.08.30
    Basic build.

---
+++ src/org/mocraft/CommandTask.java
@@ -0,0 +1,49 @@
+package org.mocraft;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by Clode on 2016/8/29.
+ */
+public class CommandTask implements Runnable {
+
+    private int id;
+    private Player player;
+    private String command = "";
+    private int time;
+
+    public CommandTask(Player player, String time, String[] args) {
+        this.player = player;
+        this.time = Integer.valueOf(time) * 60 * 20;
+        for(int i = 1; i < args.length; ++i) this.command += args[i] + " ";
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+        System.out.println(command);
+    }
+
+    @Override
+    public void run() {
+        if(time == 0) {
+            Bukkit.getScheduler().cancelTask(id);
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("add", "del"));
+        } else {
+            time--;
+        }
+        System.out.println(time);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    private int time2Tick(String time) {
+        // (y)ear, (M)onth, (d)ay, (h)our, (m)inutes, (s)econd
+
+        return 0;
+    }
+}
---
+++ src/org/mocraft/PerMession.java
@@ -0,0 +1,40 @@
+package org.mocraft;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Iterator;
+
+/**
+ * Created by Clode on 2016/8/29.
+ */
+public class PerMession extends JavaPlugin {
+
+
+
+    @Override
+    public void onEnable() {
+        Iterator<Permission> per = Bukkit.getPluginManager().getPermissions().iterator();
+        while(per.hasNext()) {
+            System.out.println(per.next().getName());
+        }
+    }
+
+    @Override
+    public void onDisable() {}
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("per")) {
+            CommandTask task = new CommandTask((Player) sender, args[0], args);
+            task.setId(Bukkit.getScheduler().scheduleSyncRepeatingTask(this, task, 0L, 1L));
+            return true;
+        }
+        return false;
+    }
+
+}
